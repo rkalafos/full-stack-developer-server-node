57199abb99e78eb400b29b14342cc94f0a823be2
Rachel Kalafos <kalafos.r@northeastern.edu>
2022-04-06 07:54:34 -0400

    first commit

---
+++ controllers/tuits.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    "_id": "123",
+    "postedBy": {"username": "NASA"},
+    "tuit": "In 2021, our @NASAPersevere Mars rover landed",
+    "likes": 345,
+    "liked": false
+  },
+  {
+    "_id": "234",
+    "postedBy": { "username": "NASA" },
+    "tuit": "Ingenuity helicopter takes flight.",
+    "likes": 346785,
+    "liked": true
+  }
+];
