bd8b819a3ca8b259fbfcffa8c64804e46f7ddb49
Rachel Kalafos <kalafos.r@northeastern.edu>
2022-04-15 22:13:23 -0400

    add db_connection_string variable

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ import userController   from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://rkalafos:<thisisapassword>@cluster0.dbfys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/webdev'
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb+srv://rkalafos:<thisisapassword>@cluster0.dbfys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 
 const app = express();
 app.use(express.json());
